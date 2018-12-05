681b74dceb26f44f4638c01f7f9f217fd3e8697b
Vojtech Curda <vicix10@gmail.com>
2018-12-05 13:35:28 +0100

    index created

---
+++ index.php
@@ -0,0 +1,19 @@
+<? header("Content-Type: text/html; charset=UTF-8");?>
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+
+<head>
+    <title> Výukový program ptactva ÈR </title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+
+</head>
+
+
+<body>
+
+</body>
+
+</html>
